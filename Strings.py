42cf897874cc56661eb27cda8d24e9528170dd41
siddharth007-singh <siddharth.code.cws@gmail.com>
2022-12-22 17:41:04 +0530

    Thrid Commit

---
+++ Strings.py
@@ -0,0 +1,7 @@
+name = "Tony Stark"
+
+# print(name.upper())
+# print(name.find('S'))  //if got return index else return -1
+# print(name.replace("Stark","Kakkar"))
+# print('S' in name) //This "In" keyword use to find the value
+
---
+++ Type_Conversion.py
@@ -0,0 +1,11 @@
+# old_age = input("Enter Your Old Age : ")
+# int(old_age)
+# new_age = old_age+2
+# print(new_age)
+
+
+# frist_no = input("Enter 1st Number: ")
+# secound_no = input("Enter 2st Number: ")
+
+# sum = int(frist_no) + int(secound_no)
+# print(sum)
