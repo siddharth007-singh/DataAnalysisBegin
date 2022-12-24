3bccb64a85650c40775e2ae34a88047af228486f
siddharth007-singh <siddharth.code.cws@gmail.com>
2022-12-24 20:45:02 +0530

    New Commit

---
+++ Pro1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas pd
+
+dict1 = {
+    "name": ['Siddharth', 'Nitika','Ravi'],
+    "marks": [92, 82, 72],
+    "city": ["Purnea", "Patna", "Bhagalpur"]
+}
+
+df = pd.DataFrame(dict1)
